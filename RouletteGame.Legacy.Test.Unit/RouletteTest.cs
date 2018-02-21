f175882347651d860a43dba6bc07c3a5651c7686
LineBD <linebdanielsen@gmail.com>
2018-02-21 14:47:46 +0100

    Opsat til Roulette

---
+++ RouletteGame.Legacy.Test.Unit/RouletteTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace RouletteGame.Legacy.Test.Unit
+{
+    [TestFixture]
+    class RouletteTest
+    { 
+        private IRoulette _uut;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _uut = Substitute.For<IRoulette>();
+        }
+
+        [Test]
+        public void GetRandomNumber_Spin_CorrectResult()
+        {
+           
+        }
+    }
+}
